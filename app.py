4aeaf675b7633e81d12cb72db7c31d7e5b93ec39
Aamil <aamilnakhuda@gmail.com>
2025-04-02 23:48:27 +0530

    2nd commit

--- app.py
+++ app.py
@@ -120,5 +120,8 @@ def shutdown():
     return "Server shutting down..."
 
 # 7. Run the Flask Application
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
